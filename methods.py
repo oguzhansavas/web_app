e8ee15c2a4c9bb5fb94c9c52599289484ccaf774
Oguzhan Savas <oguzhansavas359@gmail.com>
2025-05-12 14:03:30 +0200

    methods class

--- methods.py
+++ methods.py
@@ -1,10 +1,26 @@
 import pandas as pd
+import logging
 
-def nan_handling(self, data, method):
-    """
-    NaN handling based on user preference.
-    Args:
-        data    (df) : Dataframe containing fetched time series data.
-        method  (str): Prefered method to handle nans. 
-    Returns:
-    """
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class methods():
+    def __init__(self) -> None:
+        pass
+
+
+    def nan_handling(self, data, method):
+        """
+        NaN handling based on user preference.
+        Args:
+            data    (df) : Dataframe containing fetched time series data.
+            method  (str): Prefered method to handle nans. 
+        Returns:
+            pd.DataFrame: DataFrame with all the nans handled.
+        """
+        if not data:
+            logger.error("Invalid dataframe.")
+            return None
+        
+        
